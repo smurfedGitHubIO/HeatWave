aece28862ae196416450e39d28a43efa5fed39a5
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-08-26 21:13:17 +0800

    fix: edit content

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # HeatWave
 
-Geographical mapping for heatwave occurrences in different US states.
+Geographical mapping for heatwave occurrences in different US states embedded in a Dash app.
 
 ![Sample Map](test.png "Sample Map")
--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = pd.read_csv('./data.txt')
 
 app = Dash(__name__)
 
+features = ['Heat Wave Days Based on Daily Maximum Temperature',
+            'Heat Wave Days Based on Daily Maximum Heat Index',
+            'Heat Wave Days Based on Net Daily Heat Stress',
+            'Population']
+
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(features, 'Heat Wave Days Based on Daily Maximum Temperature', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
 
@@ -17,8 +26,11 @@ app.layout = html.Div([
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+    return px.choropleth(df, geojson=counties, locations='County Code', color=value,
+                           color_continuous_scale="Viridis",
+                           scope="usa",
+#                            labels={'unemp':'unemployment rate'}
+                          )
 
 if __name__ == '__main__':
     app.run(debug=True)
